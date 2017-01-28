d2df836c1588954c2a14f7d49f5344cf4232236d
nandck <nandck@gmail.com>
2017-01-28 15:05:23 +0000

    Update datastore.py

--- abstract_classes/datastore.py
+++ abstract_classes/datastore.py
@@ -2,7 +2,7 @@ class Datastore:
     """Used for storing and querying data by location and time."""
 
     def get(location, time, radius=DEFAULT_RADIUS, type="IMAGE"):
-        """Retrieves data for a certain location and time. Optional parameters include raidus from location and type of data to retrieve."""
+        """Retrieves data for a certain location and time. Optional parameters include radius from location and type of data to retrieve."""
     
     def put(data, location, time, type="IMAGE"):
         """Puts data at a certain location and time. Optional parameter includes type of data to store."""
