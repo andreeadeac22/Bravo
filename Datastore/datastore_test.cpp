cb7a33431efbf3907cfbd31368516824e5bf5418
Nand Kishore <nandck@gmail.com>
2017-02-15 23:00:34 +0000

    Add comments to datastore_test.cpp.

--- Datastore/datastore_test.cpp
+++ Datastore/datastore_test.cpp
@@ -6,10 +6,13 @@ int main()
 {
     Datastore db;
     db.connect("test_dump");
+    // Test raw sql query for retrieving all points.
     pqxx::result res = db.run_query("SELECT id, col, row, surface, thickness, bed, ST_AsText(geom) FROM points");
     std::cout << "Found " << res.size() << " rows." << std::endl;
+    // Test retrieving all points.
     vector<DbData> data_all = db.getAllPoints();
     std::cout << "Found " << data_all.size() << " rows." << std::endl;
+    // Test retrieving all points inside rectangle.
     vector<DbData> data_rectangle = db.getPointsInRectangle(2592, 1935, 2669, 1976);
     std::cout << "Found " << data_rectangle.size() << " rows." << std::endl;
 }
