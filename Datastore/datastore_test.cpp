9cbffa5afe7faf4ef8d1a5e04c9013aeee6aa9b8
Nand Kishore <nandck@gmail.com>
2017-02-15 22:19:09 +0000

    Modifying datastore_test.cpp.

--- Datastore/datastore_test.cpp
+++ Datastore/datastore_test.cpp
@@ -6,6 +6,8 @@ int main()
 {
     Datastore db;
     db.connect("test_dump");
+    pqxx::result res = db.run_query("SELECT id, col, row, surface, thickness, bed, ST_AsText(geom) FROM points");
+    std::cout << "Found " << res.size() << " rows." << std::endl;
     vector<DbData> data_all = db.getAllPoints();
     std::cout << "Found " << data_all.size() << " rows." << std::endl;
     vector<DbData> data_rectangle = db.getPointsInRectangle(2592, 1935, 2669, 1976);
