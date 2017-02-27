1b007a21c6955825f6b668184e659a6b8e8e0086
Matthew Else <matthewelse1997@gmail.com>
2017-02-27 11:37:29 +0000

    Add a decompression script for our snappy files

---
+++ Preprocessing/decompress.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""
+Decompress a snappy file, in the format that we're storing it. This is because
+we're using a slightly different format to the snappy command line tool.
+"""
+import sys
+
+from snappy import uncompress
+
+if __name__ == "__main__":
+    _if = sys.argv[1]
+    of = sys.argv[2]
+
+    with open(_if, 'rb') as f:
+        data = f.read()
+        decomp = uncompress(data)
+        with open(of, 'wb') as oof:
+            oof.write(decomp)
