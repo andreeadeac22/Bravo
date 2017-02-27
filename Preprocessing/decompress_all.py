1ea99ba21cdf82c0e2880703683337970287b05b
Andreea-Ioana Deac <andreeadeac22@gmail.com>
2017-02-27 23:03:08 +0000

    Add script to decompress all snappy files Additional work on smoothen.py - bug regarding value of heights

---
+++ Preprocessing/decompress_all.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""
+    Decompress a snappy file, in the format that we're storing it. This is because
+    we're using a slightly different format to the snappy command line tool.
+    
+    Usage: ./decompress.py <in file> <out file>
+    """
+import sys
+import os.path
+
+from snappy import uncompress
+
+if __name__ == "__main__":
+    for u in range(0, 25500, 500):
+        for v in range( 1000, 20500, 500):   # iterate through all snappy files in data directory
+            file= 'data/(%d, %d).snappy' % (u,v)
+            if os.path.isfile(file):
+                _if = file
+                of = 'decomp_data/(%d,%d)' % (u,v) #decompress in decomp_data directory
+                with open(_if, 'rb') as f:
+                    data = f.read()
+                    decomp = uncompress(data)
+                    with open(of, 'wb') as oof:
+                        oof.write(decomp)
--- Preprocessing/smoothen.py
+++ Preprocessing/smoothen.py
@@ -1,30 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+    This file iterates through all the decompressed snappy files (which are stored in decomp_data directory after running decompress_all.py) and all the points in each file. For each consecutive pair of height, it computes the difference and if it is too close, it deletes the point, while if it is too far, it adds additional points with heights in that interval. 
+"""
 
 import sys
 import numpy as np
+import math
+import os.path
 
 from pg import DB
+from PIL import Image
 
-def delete_point(x,y):
+def delete_point(id, surface,no_del):
+    no_del = no_del +1
+    """
+    print id, surface, "delete"
     sql = 'DELETE * FROM data WHERE row= %d AND col = %d'
     sql = sql % (x,y)
 
     db.query(sql)
+    """
 
 
-def insert_point(row,col,surface):
+def insert_point(id, surface, no):
+    no = no +1
+    print id, surface, "insert"
+    """
     sql = 'INSERT INTO data(id,row,col,surface) VALUES (%d,%d,%d,%f)'
     sql = sql % (_id, row, col, surface)
-    
+    with open('data/(%d, %d).txt' % (x, y), 'wb') as f:
+        f.write(surface)
     db.query(sql)
+    """
 
+no_steps= 10
+db = DB(dbname='grpproj', host='localhost', user=sys.argv[1])
+min_threshold = 50
+max_threshold = 1
+width = height = 500
 
-for i in range(0,width):
-    for j in range(0,height):
-    if !first:
-        y=surface[i,j]
-        if(y-x > max_threshold)
-            step_size = (y-x)/no_steps
-            for step in range(x + step_size,y, step_size)
-                insert_point(i,j,step)
-        else if(y-x < min_threshold)
-            delete_point(i,j)
+total_no =0
+
+#file = sys.argv[1]
+
+for u in range(0, 25500, 500):           #iterate through all decompressed files
+    for v in range( 1000, 20500, 500):
+        no=0
+        no_del =0
+        file = 'decomp_data/(%d,%d)' % (u,v)  # check if file exists
+        if os.path.isfile(file):
+            data = np.fromfile(file)   #put data from file in array
+            for i in range(0,len(data)):
+                    if i>0:
+                        y=data[i]
+                        if not math.isnan(y):
+                            y= int(y)
+                            if y-x > max_threshold:   # close to edge
+                                step_size = int((y-x)/no_steps)
+                                for step in range(x + step_size,y, step_size): #add intermediary values for higher density
+                                    insert_point(i,step,no)
+                            else:
+                                if y-x < min_threshold:
+                                    delete_point(i,y, no_del) # nothing happens, can have sparse data
+                    x=data[i]
+                    if not math.isnan(x):
+                        x= int(x)
+            if no > 0:
+                total_no = total_no +1
+print total_no
