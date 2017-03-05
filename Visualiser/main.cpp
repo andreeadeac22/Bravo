2496d8915204bbf2126d9af3c0bc31428de12b54
PandaJules <yb259@cam.ac.uk>
2017-03-05 14:55:43 +0000

    Update main.cpp

--- Visualiser/main.cpp
+++ Visualiser/main.cpp
@@ -1,10 +1,3 @@
-
-/**********************
-Tutorial 12: The shadow mapping effect in OSG
-COpyright (c) Franclin Foping franclin@netcourrier.com
-*////////////////////////
-
-
 #include <osg/Texture2D>
 
 #include <osg/Material>
@@ -26,6 +19,8 @@ COpyright (c) Franclin Foping franclin@netcourrier.com
 #include <osgUtil/Simplifier>
 
 #include <osgGA/TerrainManipulator>
+#include <osgGA/FlightManipulator>
+
 
 #include "util/Array2d.h"
 #include "util/log.h"
@@ -55,15 +50,7 @@ int main()
 
     scene->addChild(ls);
 
-    // material
-    ref_ptr<Material> material = new Material;
-    material->setColorMode(Material::DIFFUSE);
-    material->setAmbient(Material::FRONT_AND_BACK, Vec4(0.1, 0.1, 0.1, 1));
-    material->setSpecular(Material::FRONT_AND_BACK, Vec4(1, 1, 1, 1));
-    material->setShininess(Material::FRONT_AND_BACK, 64);
-    scene->getOrCreateStateSet()->setAttributeAndModes(material.get(), StateAttribute::ON);
-
-    //Tiled scene stuff
+    //Tiled scene
     int tile_count = 64;
     float tile_width = 80.0f;
     Array2D<TerrainTile::TileType> types(tile_count, tile_count);
@@ -80,51 +67,51 @@ int main()
 
     viewer.setSceneData(scene.get());
 
-    //Stats Event Handler s key
+    //Stats Event Handler's key
     viewer.addEventHandler(new osgViewer::StatsHandler);
-
-    // add the state manipulator
-    viewer.addEventHandler( new osgGA::StateSetManipulator(viewer.getCamera()->getOrCreateStateSet()) );
 
     //Windows size handler
     viewer.addEventHandler(new osgViewer::WindowSizeHandler);
 
     osgGA::TerrainManipulator* terrainMan = new osgGA::TerrainManipulator();
     viewer.setCameraManipulator(terrainMan);
-    //terrainMan->setTransformation(Vec3(10.0, 10.0, 10.0), Vec3(0,0,0), Vec3(0, 1.0f, 0));
 
     Vec3 center(tile_width * tile_count * 0.5f, tile_width * tile_count * 0.5f, 30.0f);
     terrainMan->setHomePosition(center + Vec3(2000.0f, 2000.0f, 2000.0f), center, Vec3(0, 0, 1.0f));
 
-    //return (viewer.run());
-
     ref_ptr<KeyboardController> keyboardCtrl(new KeyboardController);
     viewer.addEventHandler(keyboardCtrl);
 
+    //set up windows and associated threads
     viewer.realize();
+    Vec3d cam_eye;
+    Vec3d cam_center;
+    Vec3d cam_up;
 
     while (!viewer.done()) {
-        Vec3d cam_eye = Vec3d(0,0,0);
-        Vec3d cam_center = Vec3d(0,0,0);
-        Vec3d cam_up = Vec3d(0,0,0);
+
         terrainMan->getTransformation(cam_eye, cam_center, cam_up);
 
         Vec3d moveVec(0,0,0);
         if (keyboardCtrl->isKeyDown(osgGA::GUIEventAdapter::KEY_Up)) {
-            moveVec += Vec3d(1.0, 0.0, 0.0);
+            moveVec += Vec3d(1, 0, 0);
         } else if (keyboardCtrl->isKeyDown(osgGA::GUIEventAdapter::KEY_Down)) {
-            moveVec += Vec3d(-1.0, 0.0, 0.0);
+            moveVec += Vec3d(-1, 0, 0);
         } else if (keyboardCtrl->isKeyDown(osgGA::GUIEventAdapter::KEY_Left)) {
-            moveVec += Vec3d(0.0, -1.0, 0.0);
+            moveVec += Vec3d(0, 1, 0);
         } else if (keyboardCtrl->isKeyDown(osgGA::GUIEventAdapter::KEY_Right)) {
-            moveVec += Vec3d(0.0, 1.0, 0.0);
+            moveVec += Vec3d(0, -1, 0);
+        } else if (keyboardCtrl->isKeyDown(osgGA::GUIEventAdapter::KEY_Space)) {
+            // need code to return to the start position
+             //cam_center = center + Vec3(2000, 2000, 2000);
+             //cam_eye = center;
+             //cam_up = Vec3(0, 0, 200);
         }
 
-        if (moveVec != Vec3d(0.0, 0.0, 0.0)) {
-            cam_center += moveVec * 20.0f;
+        cam_center += moveVec * 20.0f;
+        cam_eye += moveVec * 20.0f;
 
-            terrainMan->setTransformation(cam_eye, cam_center, cam_up);
-        }
+        terrainMan->setTransformation(cam_eye, cam_center, cam_up);
 
         tiledScene->updateCameraPosition(cam_center);
 
