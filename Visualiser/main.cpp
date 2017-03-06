9543022b3191bb8813a26a9069ef687fc889b4fc
James Lomax <james.lmx@gmail.com>
2017-03-06 04:20:16 +0000

    Merged changes to main file

--- Visualiser/main.cpp
+++ Visualiser/main.cpp
@@ -1,10 +1,3 @@
-
-/**********************
-Tutorial 12: The shadow mapping effect in OSG
-COpyright (c) Franclin Foping franclin@netcourrier.com
-*////////////////////////
-
-
 #include <osg/Texture2D>
 
 #include <osg/Material>
@@ -26,6 +19,9 @@ COpyright (c) Franclin Foping franclin@netcourrier.com
 #include <osgUtil/Simplifier>
 
 #include <osgGA/TerrainManipulator>
+#include <osgGA/FlightManipulator>
+
+ #include <QMediaPlayer>
 
 #include "util/Array2d.h"
 #include "util/log.h"
@@ -55,14 +51,6 @@ int main()
 
     scene->addChild(ls);
 
-    // material
-    ref_ptr<Material> material = new Material;
-    material->setColorMode(Material::DIFFUSE);
-    material->setAmbient(Material::FRONT_AND_BACK, Vec4(0.1, 0.1, 0.1, 1));
-    material->setSpecular(Material::FRONT_AND_BACK, Vec4(1, 1, 1, 1));
-    material->setShininess(Material::FRONT_AND_BACK, 64);
-    scene->getOrCreateStateSet()->setAttributeAndModes(material.get(), StateAttribute::ON);
-
     //Tiled scene stuff
     float tile_width = 128.0f;
     Array2D<TerrainTile::TileType> types(34, 34);
@@ -73,13 +61,13 @@ int main()
     }
 
     ref_ptr<TiledScene> tiledScene(new TiledScene(types, tile_width));
-    tiledScene->setRenderDistance(6.0f);
+    tiledScene->setRenderDistance(20.0f);
 
     scene->addChild(tiledScene->getNode());
 
     viewer.setSceneData(scene.get());
 
-    //Stats Event Handler s key
+    //Stats Event Handler's key
     viewer.addEventHandler(new osgViewer::StatsHandler);
 
     // add the state manipulator
@@ -90,12 +78,11 @@ int main()
 
     osgGA::TerrainManipulator* terrainMan = new osgGA::TerrainManipulator();
     viewer.setCameraManipulator(terrainMan);
-    //terrainMan->setTransformation(Vec3(10.0, 10.0, 10.0), Vec3(0,0,0), Vec3(0, 1.0f, 0));
 
-    Vec3 t_center(2895.55, 2837, 26.7789);
-    Vec3 t_eye(2584.64, 2526.09, 337.695);
-    //Vec3 t_center(0,0,0);
-    //Vec3 t_eye(-500, -500, 500);
+//    Vec3 t_center(2895.55, 2837, 26.7789);
+//    Vec3 t_eye(2584.64, 2526.09, 337.695);
+    Vec3 t_center(0,0,0);
+    Vec3 t_eye(-500, -500, 500);
     terrainMan->setHomePosition(t_eye, t_center, Vec3(0, 0, 1.0f));
 
     //return (viewer.run());
@@ -103,30 +90,36 @@ int main()
     ref_ptr<KeyboardController> keyboardCtrl(new KeyboardController);
     viewer.addEventHandler(keyboardCtrl);
 
+    //set up windows and associated threads
     viewer.realize();
+    Vec3d cam_eye;
+    Vec3d cam_center;
+    Vec3d cam_up;
 
     int cameraUpdateFrame = 20;
 
+    //play background music
+    QMediaPlayer *music = new QMediaPlayer();
+    music->setMedia(QUrl("qrc:/sounds/wind.wav"));
+    music->play();
+
     while (!viewer.done()) {
-        Vec3d cam_eye = Vec3d(0,0,0);
-        Vec3d cam_center = Vec3d(0,0,0);
-        Vec3d cam_up = Vec3d(0,0,0);
         terrainMan->getTransformation(cam_eye, cam_center, cam_up);
 
         Vec3d moveVec(0,0,0);
         if (keyboardCtrl->isKeyDown(osgGA::GUIEventAdapter::KEY_Up)) {
-            moveVec += Vec3d(1.0, 0.0, 0.0);
+            moveVec += Vec3d(1, 0, 0);
         } else if (keyboardCtrl->isKeyDown(osgGA::GUIEventAdapter::KEY_Down)) {
-            moveVec += Vec3d(-1.0, 0.0, 0.0);
+            moveVec += Vec3d(-1, 0, 0);
         } else if (keyboardCtrl->isKeyDown(osgGA::GUIEventAdapter::KEY_Left)) {
-            moveVec += Vec3d(0.0, -1.0, 0.0);
+            moveVec += Vec3d(0, 1, 0);
         } else if (keyboardCtrl->isKeyDown(osgGA::GUIEventAdapter::KEY_Right)) {
-            moveVec += Vec3d(0.0, 1.0, 0.0);
-        }
-
-        if (keyboardCtrl->isKeyDown(osgGA::GUIEventAdapter::KEY_K)) {
-            log_info << "Pos: " << cam_center << std::endl;
-            log_info << "Eye: " << cam_eye << std::endl;
+            moveVec += Vec3d(0, -1, 0);
+        } else if (keyboardCtrl->isKeyDown(osgGA::GUIEventAdapter::KEY_Space)) {
+            // need code to return to the start position
+             //cam_center = center + Vec3(2000, 2000, 2000);
+             //cam_eye = center;
+             //cam_up = Vec3(0, 0, 200);
         }
 
         if (moveVec != Vec3d(0.0, 0.0, 0.0)) {
